f3d9aca8faac3fb0e59e22b1ea79f99bbbd0791b
mnmcsherry <72521310+mnmcsherry@users.noreply.github.com>
2021-02-19 23:51:48 -0800

    Prototype 2_MM
    My challenge is in the same simulation as my prototype.

---
+++ Prototype 2_MM/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    private float lowerBound = -10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If an object goes past the players view in the game, remove that object
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
