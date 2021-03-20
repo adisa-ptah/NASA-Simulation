70318a6c77788d9b342d50ef7dadd7d064280636
mnmcsherry <72521310+mnmcsherry@users.noreply.github.com>
2021-03-20 11:19:27 -0700

    3.20 Work

---
+++ 3.20 Work/Assets/RobotSphere/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float speed = 15.0f;
+    private float turnSpeed = 30.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Move the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
+
