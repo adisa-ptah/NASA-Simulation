726912e7116f3a5fcdfc47018cef31d3a271d11e
Ojureyrey <67301837+Ojureyrey@users.noreply.github.com>
2021-05-29 21:59:22 +0100

    commented possible camera alternative

--- Environment Terrain 4.28/Assets/Scripts/CameraController.cs
+++ Environment Terrain 4.28/Assets/Scripts/CameraController.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour
+
 {
 
     public GameObject player;        //Public variable to store a reference to the player game object
@@ -23,3 +25,34 @@ public class CameraController : MonoBehaviour
         transform.position = player.transform.position + offset;
     }
 }
+
+
+/*
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Third person Camera
+        Camera.main.transform.position = this.transform.position - this.transform.forward * 45 + this.transform.up * 30;
+        //Camera.main.transform.LookAt(this.transform.position);
+        Camera.main.transform.parent = this.transform;
+
+        // First person Camera
+        //Camera.main.transform.position = this.transform.position - this.transform.forward * -1.0f + this.transform.up * 1.0f; 
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+*/
+
+
+
