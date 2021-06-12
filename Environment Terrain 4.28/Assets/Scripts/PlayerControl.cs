bdac1442aadcc7bcbc347907dcbc5b063483e65a
adisa-ptah <72063240+adisa-ptah@users.noreply.github.com>
2021-06-12 09:51:28 -0700

    Adisa's Updates 6.12.21

--- Environment Terrain 4.28/Assets/Scripts/PlayerControl.cs
+++ Environment Terrain 4.28/Assets/Scripts/PlayerControl.cs
@@ -8,12 +8,12 @@ public class PlayerControl : MonoBehaviour
     public float speed = 20.0f;
     public float turnSpeed = 45.0f;
     public float horizontalInput;
-    public float forwardInput;
+    public float forwardInput;  
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
