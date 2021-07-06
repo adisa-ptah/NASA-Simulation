d9ad52889510a53888516e8225188f92d308ef6d
alexlizikhin <alex.lizikhin.505@my.csun.edu>
2021-07-06 09:57:49 -0700

    Wall_Update
    Hank has been added and walls seem to be solid.

--- Environment Terrain 4.28/Assets/Scripts/FollowPlayer.cs
+++ Environment Terrain 4.28/Assets/Scripts/FollowPlayer.cs
@@ -1,9 +1,12 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, 9);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Offset the camera behind the player by adding to the player's position. 
+        transform.position = player.transform.position + offset;
     }
 }
