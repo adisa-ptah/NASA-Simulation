bebd3a6e49f26145fd8cfd018fa89072422e5fb2
alexlizikhin <alex.lizikhin.505@my.csun.edu>
2021-08-19 20:34:24 -0700

    Floor_Update
    Added a physical floor/roof under the map so that the player cannot see above the surface while underground. Also added file to main branch.

--- Honey I'm Lost on Mars/Assets/Scripts/FollowPlayer.cs
+++ Honey I'm Lost on Mars/Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
-    private Vector3 offset = new Vector3(-3, 7, 10);
+    private Vector3 offset = new Vector3(-3, 7, 8);
     // Start is called before the first frame update
     void Start()
     {
